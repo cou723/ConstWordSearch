35439628a2fd92bf7e8d183fc380b07a9596d23b
courange <courange.c@gmail.com>
2021-02-26 12:36:57 +0900

    デザインの修正

--- search.js
+++ search.js
@@ -24,15 +24,15 @@ console.log(inputs);
 for (let _i = 0; _i < inputs.length; _i++) {
     console.log(_i);
     console.log(inputs[_i]);
-    inputs[_i].addEventListener('keydown',(e) => {
-        if(e.keyCode === 13){
+    inputs[_i].addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
             console.log("click");
             Search(_i);
         }
     });
 }
 
-function Search(index){
+function Search(index) {
     var header = document.getElementById(`headerText${index}`).innerText;
     var input = document.getElementById(`${index}`).value;
     console.log(`headerText${index}[${header}]`);
@@ -49,7 +49,7 @@ function Search(index){
 
 function addSearchObj(headerText) {
     var count = searchesArea.children.length;
-    console.log("call:addSearchObj:" + headerText );
+    console.log("call:addSearchObj:" + headerText);
 
     //div生成
     var searchDiv = document.createElement("div");
@@ -57,38 +57,49 @@ function addSearchObj(headerText) {
     searchDiv.classList.add("row");
     searchesArea.appendChild(searchDiv);
 
+    //form生成
+    var searchForm = document.createElement("form");
+    searchForm.id = `searchForm${count}`;
+    searchForm.onsubmit = 'return false';
+    searchForm.setAttribute('onsubmit', 'return false');
+    searchForm.classList.add("p-0");
+    //searchForm.classList.add("col-10");
+    searchForm.classList.add("mb-2");
+    searchForm.classList.add("input-group");
+    searchDiv.appendChild(searchForm);
+
     //p生成
     var searchP = document.createElement("p");
     searchP.innerText = headerText;
     searchP.id = `headerText${count}`;
     searchP.classList.add("headerText");
     searchP.classList.add("col-2");
+    searchP.classList.add("mb-0");
+    searchP.classList.add("align-items-center");
+    searchP.classList.add("d-flex");
+    searchP.classList.add("flex-row-reverse");
+    searchP.classList.add("input-group-addon");
     //searchP.classList.add("border-right-0");
-    searchP.classList.add("border-primary");
+    searchP.classList.add("border");
+    searchP.classList.add("rounded-left");
     //searchP.classList.add("rounded-left");
-    searchDiv.appendChild(searchP);
+    searchForm.appendChild(searchP);
 
-    //form生成
-    var searchForm = document.createElement("form");
-    searchForm.id = `searchForm${count}`;
-    searchForm.onsubmit = 'return false';
-    searchForm.setAttribute('onsubmit','return false');
-    searchForm.classList.add("p-0");
-    searchForm.classList.add("col-10");
-    searchDiv.appendChild(searchForm);
+
 
     //input生成
     var searchInput = document.createElement("input");
     searchInput.id = `${count}`;
     searchInput.type = "text";
     searchInput.classList.add("form-control");
+    searchInput.classList.add("mb-0");
+    searchInput.classList.add("rounded-right");
     searchForm.appendChild(searchInput);
 
-    //button生成
-    //var searchButton = document.createElement("input");
-    //searchButton.value = "検索";
-    //searchButton.type = "submit";
-    //searchButton.id = `${count}`;
-    //searchInput.classList.add("form-control");
-    //searchForm.appendChild(searchButton);
+    /**
+     *  <div class="input-group">
+            <span class="input-group-addon">Text</span>
+            <input id="msg" type="text" class="form-control" name="msg" placeholder="Additional Info">
+        </div>
+     */
 }
