07156a66ada673a7380cbfb7dd267b869651df52
cou <courange.c@gmail.com>
2022-05-31 22:33:02 +0900

    copy urlの機能の削除

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
 import $ from "jquery";
 import { addSearchBox } from "./searchBox";
 import { init } from "./init";
-import { copyText } from "./copyURL.js";
 import { addWordStorage } from "./storage";
 
 $("#add-word-button").on("click", addButtonClick);
-$("#copy-button").on("click", copyText);
 init();
 //dataReturnProcess();
 
